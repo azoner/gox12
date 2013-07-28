d86646e9e4755fa51abfc62045a88aade8737f68
John Holland <azoner@gmail.com>
2013-07-27 23:58:10 -0400

    go fmt

--- rawx12file.go
+++ rawx12file.go
@@ -1,70 +1,68 @@
 package gox12
 
 import (
-    "bufio"
-    //"bytes"
-    "io"
-    "fmt"
-    "strings"
-    "log"
+	"bufio"
+	//"bytes"
+	"fmt"
+	"io"
+	"log"
+	"strings"
 )
 
 func ReadSegmentLines(inFile io.Reader, ch chan RawSegment) {
-    reader := bufio.NewReader(inFile)
-    //buffer := bytes.NewBuffer(make([]byte, 0))
-    first, err := reader.Peek(106)
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
-    isa := string(first)
-    delim := getDelimiters(isa)
-    fmt.Println(delim)
-    ct := 0
-    for {
-        row, err := reader.ReadString(delim.SegmentTerm)
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            panic(err)
-        }
-        row = strings.Trim(row, "~\r\n")
-        mySeg := MakeSegment(row, delim)
-        ct++
-        seg := RawSegment{
-            mySeg,
-            delim,
-            ct,
-        }
-        ch <- seg
-    }
-    close(ch)
+	reader := bufio.NewReader(inFile)
+	//buffer := bytes.NewBuffer(make([]byte, 0))
+	first, err := reader.Peek(106)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	isa := string(first)
+	delim := getDelimiters(isa)
+	fmt.Println(delim)
+	ct := 0
+	for {
+		row, err := reader.ReadString(delim.SegmentTerm)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		row = strings.Trim(row, "~\r\n")
+		mySeg := MakeSegment(row, delim)
+		ct++
+		seg := RawSegment{
+			mySeg,
+			delim,
+			ct,
+		}
+		ch <- seg
+	}
+	close(ch)
 }
 
 type RawSegment struct {
-    Segment Segment
-    Terminators Delimiters
-    LineCount int
+	Segment     Segment
+	Terminators Delimiters
+	LineCount   int
 }
 
 type Delimiters struct {
-    SegmentTerm byte
-    ElementTerm byte
-    SubelementTerm byte
-    RepetitionTerm byte
+	SegmentTerm    byte
+	ElementTerm    byte
+	SubelementTerm byte
+	RepetitionTerm byte
 }
 
 func getDelimiters(isa string) Delimiters {
-    d := Delimiters{
-        isa[len(isa)-1],
-        isa[3],
-        isa[len(isa)-2],
-        0,
-    }
-    if isa[84:89] == "005010" {
-        d.SubelementTerm = isa[82]
-    }
-    return d
+	d := Delimiters{
+		isa[len(isa)-1],
+		isa[3],
+		isa[len(isa)-2],
+		0,
+	}
+	if isa[84:89] == "005010" {
+		d.SubelementTerm = isa[82]
+	}
+	return d
 }
-
-
--- segment.go
+++ segment.go
@@ -1,17 +1,17 @@
 package gox12
 
 import (
-    "strings"
+	"strings"
 )
 
 type Segment struct {
-    SegmentId string
-    Elements []string
+	SegmentId string
+	Elements  []string
 }
 
 func MakeSegment(line string, delim Delimiters) (segment Segment) {
-    fields := strings.Split(line, string(delim.ElementTerm))
-    segment.SegmentId = fields[0]
-    segment.Elements =  fields[1:]
-    return
+	fields := strings.Split(line, string(delim.ElementTerm))
+	segment.SegmentId = fields[0]
+	segment.Elements = fields[1:]
+	return
 }
