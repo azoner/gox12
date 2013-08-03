eef5ee4a515f12823ad116d62efab36ce9ad0b03
John Holland <azoner@gmail.com>
2013-08-03 01:40:28 -0400

    go fmt

--- segment.go
+++ segment.go
@@ -2,31 +2,32 @@ package gox12
 
 import (
 	"strings"
-    //"fmt"
+	//"fmt"
 )
 
 type Segment struct {
 	SegmentId string
 	//Composites []Composite
-    Composites [][]string
+	Composites [][]string
 }
 
 //type Composite []string
 
 func NewSegment(line string, elementTerm byte, subelementTerm byte, repTerm byte) (segment Segment) {
 	fields := strings.Split(line, string(elementTerm))
-    segment.SegmentId = fields[0]
-    comps := make([][]string, len(fields)-1)
-    for i, v := range fields[1:] {
-        c := strings.Split(v, string(subelementTerm))
-        comps[i] = c
-    }
-    segment.Composites = comps
-    return
+	segment.SegmentId = fields[0]
+	comps := make([][]string, len(fields)-1)
+	for i, v := range fields[1:] {
+		c := strings.Split(v, string(subelementTerm))
+		t := make([]string, cap(c))
+		copy(t, c)
+		comps[i] = t
+	}
+	segment.Composites = comps
+	return
 }
 
-
 func splitComposite(f2 string, term string) (ret []string) {
-    ret = strings.Split(f2, term)
-    return
+	ret = strings.Split(f2, term)
+	return
 }
--- segment_test.go
+++ segment_test.go
@@ -6,9 +6,9 @@ import (
 
 // AAA
 func TestSegmentParseSegmentId(t *testing.T) {
-    str2 := "TST&AA!1!1&BB!5"
-    seg := NewSegment(str2, '&', '!', '^')
-    expectedSegId := "TST"
+	str2 := "TST&AA!1!1&BB!5"
+	seg := NewSegment(str2, '&', '!', '^')
+	expectedSegId := "TST"
 	if seg.SegmentId != expectedSegId {
 		t.Errorf("Didn't get expected result [%s], instead got [%s]", expectedSegId, seg.SegmentId)
 	}
