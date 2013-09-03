4b6c58ff007d53b42d66cd2c06e8c7c1cfe69bb0
John Holland <azoner@gmail.com>
2013-09-03 17:56:20 -0400

    update matcher func closures

--- path_finder.go
+++ path_finder.go
@@ -12,6 +12,11 @@ type EmptyPath struct {
 	Path string
 }
 
+// map[start_x12path] []struct {match_func func(seg) bool, newpath string} 
+
+//testLookups := map[string] 
+
+
 //func (e *EmptyPath) Run2 PathFinder {
 //    return "", true, nil
 //}
@@ -30,4 +35,37 @@ type EmptyPath struct {
 // need to close lookup maps
 func findPath(rawpath string, seg Segment) (newpath string, ok bool, err error) {
 	return "", true, nil
+}
+
+// is the segment "matched"
+type segMatcher func(seg Segment) bool
+
+func segmentMatchBySegmentId(segmentId string) segMatcher {
+	return func(seg Segment) bool {
+		return seg.SegmentId == segmentId
+	}
+}
+
+func segmentMatchIdByPath(segmentId string, x12path string, id_value string) segMatcher {
+	return func(seg Segment) bool {
+		v, found, _ := seg.GetValue(x12path)
+		return seg.SegmentId == segmentId && found && v == id_value
+	}
+}
+
+func segmentMatchIdListByPath(segmentId string, x12path string, id_list []string) segMatcher {
+	return func(seg Segment) bool {
+		v, found, _ := seg.GetValue(x12path)
+		x := stringInSlice(v, id_list)
+		return seg.SegmentId == segmentId && found && x
+	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
