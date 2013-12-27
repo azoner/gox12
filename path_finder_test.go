1bdbd15aa2b6f0882288899f384c6600f11bf7ee
John Holland <azoner@gmail.com>
2013-12-27 01:11:42 -0500

    update basic finder test

--- path_finder_test.go
+++ path_finder_test.go
@@ -8,12 +8,10 @@ import (
 func TestMatcherISA(t *testing.T) {
 	finder := MakeMapFinder()
 	str1 := "ISA&00&          &00&       "
-	//str2 := "TST&AA!1!1&BB!5"
 	seg := NewSegment(str1, '&', '!', '^')
-	//expectedSegId := ""
-	path1, _, _ := finder("", seg)
+	foundPath, _, _ := finder("", seg)
 	expectedPath := "/ISA_LOOP/ISA"
-	if expectedPath != path1 {
-		t.Errorf("Didn't get expected result [%s], instead got [%s]", expectedPath, path1)
+	if expectedPath != foundPath {
+		t.Errorf("Didn't get expected result [%s], instead got [%s]", expectedPath, foundPath)
 	}
 }
